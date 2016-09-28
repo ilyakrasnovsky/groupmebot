1205bd5e44931a5f49af768e99cfd9211947fef4
ilyakrasnovsky <ilyakrasnovsky@gmail.com>
2016-09-27 22:47:14 -0400

    commit to pull

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
 
 def index(request):
 	return render(request, 'bot/home.html')
+
+def boobot(request):
+	context = {"data" : "meh"}
+	if (request.method == "POST"):
+		context = request.POST.dict()
+	return JsonResponse(context)
